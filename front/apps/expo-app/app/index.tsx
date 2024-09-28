78ab9f2936f00eaaed2e0205f8cf8bef2f0141e9
Fátima Kraiczyi <fatima.kraiczyi@brf.com>
2024-09-27 21:37:00 -0300

    fix: mobile build

--- front/apps/expo-app/app/index.tsx
+++ front/apps/expo-app/app/index.tsx
@@ -1,70 +1,29 @@
-import React from "react";
-import { Button, ButtonText } from "@/components/ui/button";
+import React, { useEffect } from "react";
 import { router } from "expo-router";
 import { SafeAreaView } from "@/components/ui/safe-area-view";
-import { VStack } from "@/components/ui/vstack";
+import { Text } from "react-native";
+import SignIn from "./auth/signin";
+
 const index = () => {
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const token = await sessionStorage.getItem("x-access-token");
+      
+      if (token) {
+        // Se o token estiver presente, redireciona para o dashboard
+        router.replace("dashboard/dashboard-layout");
+      } else {
+        // Caso contrário, redireciona para a tela de login
+        router.replace("auth/signin");
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+
   return (
     <SafeAreaView className="md:flex flex-col items-center justify-center md:w-full h-full">
-      <VStack className="p-2 md:max-w-[440px] w-full" space="xl">
-        <Button
-          onPress={() => {
-            router.push("auth/splash-screen");
-          }}
-        >
-          <ButtonText>SplashScreen</ButtonText>
-        </Button>
-        <Button
-          className="w-full"
-          onPress={() => {
-            router.push("auth/signin");
-          }}
-        >
-          <ButtonText>Sign in</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("auth/signup");
-          }}
-        >
-          <ButtonText>Sign up</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("auth/forgot-password");
-          }}
-        >
-          <ButtonText>Forgot password</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("auth/create-password");
-          }}
-        >
-          <ButtonText>Create password</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("news-feed/news-and-feed");
-          }}
-        >
-          <ButtonText>News feed</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("dashboard/dashboard-layout");
-          }}
-        >
-          <ButtonText>Dashboard</ButtonText>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push("profile/profile");
-          }}
-        >
-          <ButtonText>Profile</ButtonText>
-        </Button>
-      </VStack>
+      <SignIn/>
     </SafeAreaView>
   );
 };
