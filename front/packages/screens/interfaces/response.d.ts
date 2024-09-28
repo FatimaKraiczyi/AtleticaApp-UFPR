04383c920c47c8298408b91775515ca9200bfb0e
Fátima Kraiczyi <fatima.kraiczyi@brf.com>
2024-09-28 05:01:52 -0300

    delete

--- front/packages/screens/interfaces/response.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Default<T> {
-    data: T | undefined;
-    success: boolean;
-    message?: string;
-}
-export interface Delete {
-    success: boolean;
-    message: string;
-}
--- front/packages/screens/interfaces/users.d.ts
+++
@@ -1,9 +0,0 @@
-export interface UserProps {
-    email?: string;
-    senha?: string;
-    repSenha?: string;
-    token?: string;
-    telefone?: string;
-    dataNasc?: string;
-    idCurso?: number | string;
-}
