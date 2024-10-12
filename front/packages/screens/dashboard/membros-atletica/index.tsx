acb4a5f57c2917385522ccaead8d8732d925bef2
Fátima Kraiczyi <fatima.kraiczyi@brf.com>
2024-10-12 03:26:16 -0300

    renderNoMembers

--- front/packages/screens/dashboard/membros-atletica/index.tsx
+++ front/packages/screens/dashboard/membros-atletica/index.tsx
@@ -6,6 +6,7 @@ import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
 import { Pressable } from "@/components/ui/pressable";
 import { useState } from "react";
+import { Image } from "react-native";
 import { ScrollView } from "@/components/ui/scroll-view";
 import { Grid, GridItem } from "@/components/ui/grid";
 import { SafeAreaView } from "@/components/ui/safe-area-view";
@@ -15,6 +16,7 @@ import { ModalMembros } from "./membro-modal";
 import { useMembros } from "../../../hooks/MembrosContext";
 import { LayoutComponents } from "../../components/LayoutComponents";
 import { DeleteMembro } from "./delete-membro";
+import { Center } from "@/components/ui/center";
 
 const MainContent = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -52,85 +54,99 @@ const MainContent = () => {
     setMembroEmailToDelete(null);
   };
 
+  const renderNoMembers = () => (
+    <Center className="flex-1 mt-14 justify-center items-center h-full">
+      <Image
+        source={{
+          uri: "https://img.icons8.com/external-outline-andi-nur-abdillah/100/external-Empty-empty-state-(outline)-outline-andi-nur-abdillah.png",
+        }}
+        style={{ width: 100, height: 100, tintColor: "#8E8E8E" }}
+        resizeMode="contain"
+      />
+      <Text className="text-center text-gray-500 dark:text-gray-400 mt-4">
+        Nenhum membro encontrado.
+      </Text>
+    </Center>
+  );
+
+  const renderMembers = () => (
+    <Grid _extra={{ className: "gap-5" }}>
+      {membros.map((item, index) => (
+        <GridItem
+          _extra={{
+            className: "col-span-12 sm:col-span-6 lg:col-span-4",
+          }}
+          key={index}
+        >
+          <VStack
+            space="md"
+            className="border border-border-300 rounded-lg p-4"
+          >
+            <HStack space="xl" className="items-center justify-between">
+              <HStack space="xl" className="items-center">
+                <VStack>
+                  <Text className="font-semibold text-gray-900 dark:text-gray-100 line-clamp-1">
+                    {item.Usuario.nome}
+                  </Text>
+                  <Text className="italic text-gray-700 dark:text-gray-300 line-clamp-1">
+                    {item.Usuario.email}
+                  </Text>
+                  <Text className="text-gray-500 dark:text-gray-400 line-clamp-1">
+                    {item.administrador ? "Administrador" : "Membro"}
+                  </Text>
+                </VStack>
+              </HStack>
+              <HStack space="md">
+                <Pressable
+                  onPress={() =>
+                    item.Usuario?.email !== undefined &&
+                    handleEditMembroPress(item)
+                  }
+                >
+                  <Icon as={EditIcon} className="text-typography-600" />
+                </Pressable>
+                <Pressable
+                  onPress={() =>
+                    item.Usuario?.email !== undefined &&
+                    handleOpenDeleteModal(item.Usuario.email)
+                  }
+                >
+                  <Icon as={TrashIcon} className="text-typography-600" />
+                </Pressable>
+              </HStack>
+            </HStack>
+          </VStack>
+        </GridItem>
+      ))}
+    </Grid>
+  );
+
   return (
     <Box className="flex-1">
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingBottom: isWeb ? 0 : 100,
-          flexGrow: 1,
-        }}
-        className="flex-1 mb-20 md:mb-2"
-      >
-        <VStack className="p-4 pb-0 md:px-10 md:pt-6  w-full" space="2xl">
-          <VStack space="lg" className="items-center">
-            <Button
-              className="gap-3 relative"
-              onPress={handleCadastrarMembroPress}
-            >
-              <ButtonText>Cadastrar Membro</ButtonText>
-            </Button>
-          </VStack>
-
-          <Grid _extra={{ className: "gap-5" }}>
-            {membros && membros.length > 0 ? (
-              membros.map((item, index) => (
-                <GridItem
-                  _extra={{
-                    className: "col-span-12 sm:col-span-6 lg:col-span-4",
-                  }}
-                  key={index}
-                >
-                  <VStack
-                    space="md"
-                    className="border border-border-300 rounded-lg p-4"
-                  >
-                    <HStack space="xl" className="items-center justify-between">
-                      <HStack space="xl" className="items-center">
-                        <VStack>
-                          <Text className="font-semibold text-gray-900 dark:text-gray-100 line-clamp-1">
-                            {item.Usuario.nome}
-                          </Text>
-                          <Text className="italic text-gray-700 dark:text-gray-300 line-clamp-1">
-                            {item.Usuario.email}
-                          </Text>
-                          <Text className="text-gray-500 dark:text-gray-400 line-clamp-1">
-                            {item.administrador ? "Administrador" : "Membro"}
-                          </Text>
-                        </VStack>
-                      </HStack>
-                      <HStack space="md">
-                        <Pressable
-                          onPress={() =>
-                            item.Usuario?.email !== undefined &&
-                            handleEditMembroPress(item)
-                          }
-                        >
-                          <Icon as={EditIcon} className="text-typography-600" />
-                        </Pressable>
-                        <Pressable
-                          onPress={() =>
-                            item.Usuario?.email !== undefined &&
-                            handleOpenDeleteModal(item.Usuario.email)
-                          }
-                        >
-                          <Icon
-                            as={TrashIcon}
-                            className="text-typography-600"
-                          />
-                        </Pressable>
-                      </HStack>
-                    </HStack>
-                  </VStack>
-                </GridItem>
-              ))
-            ) : (
-              <Text className="text-center">Nenhum membro encontrado.</Text>
-            )}
-          </Grid>
+      <VStack className="p-4 pb-0 md:px-10 md:pt-6 w-full" space="2xl">
+        <VStack space="lg" className="items-center">
+          <Button
+            className="gap-3 relative"
+            onPress={handleCadastrarMembroPress}
+          >
+            <ButtonText>Cadastrar Membro</ButtonText>
+          </Button>
         </VStack>
-      </ScrollView>
-
+        {membros.length === 0 ? (
+          renderNoMembers()
+        ) : (
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              paddingBottom: isWeb ? 0 : 100,
+              flexGrow: 1,
+            }}
+            className="flex-1 mb-20 md:mb-2"
+          >
+            {renderMembers()}
+          </ScrollView>
+        )}
+      </VStack>
       <ModalMembros
         showModal={isModalVisible}
         setShowModal={handleCloseModal}
