728f42d022384449c80120b6c8c15e3ee871dbe5
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2024-10-23 05:11:36 -0300

    type para acesso admin atletica

--- front/packages/screens/dashboard/dashboard-layout/index.tsx
+++ front/packages/screens/dashboard/dashboard-layout/index.tsx
@@ -53,6 +53,7 @@ const HeadingCards: CardData[] = [
     bannerUri: require("@/assets/dashboard/dashboard-layout/image6.png"),
     title: "Admin Atlética",
     description: "Set a target to accomplish",
+		userType: "ADMIN",
     route: "/dashboard/admin",
   },
   {
