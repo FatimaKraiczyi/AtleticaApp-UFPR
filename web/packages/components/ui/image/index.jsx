3e950a202689c70818d40dcb8a4162c260de2732
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2024-10-26 01:30:24 -0300

    up web

---
+++ web/packages/components/ui/image/index.jsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import UnitoolsImage from "@unitools/image";
+import { tva } from "@gluestack-ui/nativewind-utils/tva";
+import { cssInterop } from "@gluestack-ui/nativewind-utils/cssInterop";
+const imageStyle = tva({
+    base: "max-w-full",
+    variants: {
+        size: {
+            "2xs": "h-6 w-6",
+            xs: "h-10 w-10",
+            sm: "h-16 w-16",
+            md: "h-20 w-20",
+            lg: "h-24 w-24",
+            xl: "h-32 w-32",
+            "2xl": "h-64 w-64",
+            full: "h-full w-full",
+        },
+    },
+});
+const UIImage = UnitoolsImage;
+cssInterop(UIImage, 
+//@ts-ignore
+UnitoolsImage.displayName === "NextImage" ? {} : { className: "style" });
+const Image = ({ size = "md", className, ...props }) => {
+    return (<UIImage 
+    // @ts-ignore
+    className={imageStyle({ size, class: className })} {...props}/>);
+};
+Image.displayName = "Image";
+export { Image };
