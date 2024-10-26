e790e7c5d2b3f794e4b959314c38d9727b236a71
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2024-10-25 21:45:22 -0300

    fix: login

--- expo-app/screens/auth/create-password/index.tsx
+++ expo-app/screens/auth/create-password/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Toast, ToastTitle, useToast } from "@/components/ui/toast";
 import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
 import {
   FormControl,
   FormControlError,
@@ -21,40 +22,27 @@ import { AlertTriangle } from "lucide-react-native";
 import { Pressable } from "@/components/ui/pressable";
 import useRouter from "@unitools/router";
 import { AuthLayout } from "../layout";
-import { newPassword } from "../../../../api/users";
 
 const createPasswordSchema = z.object({
   password: z
     .string()
-    .min(8, "A senha deve ter no mínimo 8 caracteres")
-    .regex(
-      new RegExp(".*[A-Z].*"),
-      "Deve conter pelo menos uma letra maiúscula"
-    )
-    .regex(
-      new RegExp(".*[a-z].*"),
-      "Deve conter pelo menos uma letra minúscula"
-    )
-    .regex(new RegExp(".*\\d.*"), "Deve conter pelo menos um número")
+    .min(6, "Must be at least 8 characters in length")
+    .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
+    .regex(new RegExp(".*[a-z].*"), "One lowercase character")
+    .regex(new RegExp(".*\\d.*"), "One number")
     .regex(
       new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
-      "Deve conter pelo menos um caractere especial"
+      "One special character"
     ),
   confirmpassword: z
     .string()
-    .min(8, "A senha deve ter no mínimo 8 caracteres")
-    .regex(
-      new RegExp(".*[A-Z].*"),
-      "Deve conter pelo menos uma letra maiúscula"
-    )
-    .regex(
-      new RegExp(".*[a-z].*"),
-      "Deve conter pelo menos uma letra minúscula"
-    )
-    .regex(new RegExp(".*\\d.*"), "Deve conter pelo menos um número")
+    .min(6, "Must be at least 8 characters in length")
+    .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
+    .regex(new RegExp(".*[a-z].*"), "One lowercase character")
+    .regex(new RegExp(".*\\d.*"), "One number")
     .regex(
       new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
-      "Deve conter pelo menos um caractere especial"
+      "One special character"
     ),
 });
 
@@ -70,60 +58,51 @@ const CreatePasswordWithLeftBackground = () => {
     resolver: zodResolver(createPasswordSchema),
   });
   const toast = useToast();
-  const router = useRouter();
 
-  const onSubmit = async (data: CreatePasswordSchemaType) => {
-    if (data.password !== data.confirmpassword) {
+  const onSubmit = (data: CreatePasswordSchemaType) => {
+    if (data.password === data.confirmpassword) {
       toast.show({
         placement: "bottom right",
-        render: ({ id }) => (
-          <Toast nativeID={id} variant="solid" action="error">
-            <ToastTitle>Senhas não correspondem</ToastTitle>
-          </Toast>
-        ),
-      });
-      return;
-    }
-
-    const response = await newPassword(data.password, data.confirmpassword);
-
-    if (response.success) {
-      toast.show({
-        placement: "bottom right",
-        render: ({ id }) => (
-          <Toast nativeID={id} variant="solid" action="success">
-            <ToastTitle>Senha criada com sucesso</ToastTitle>
-          </Toast>
-        ),
+        render: ({ id }) => {
+          return (
+            <Toast nativeID={id} variant="solid" action="success">
+              <ToastTitle>Success</ToastTitle>
+            </Toast>
+          );
+        },
       });
       reset();
-      router.push("/auth/signin");
     } else {
       toast.show({
         placement: "bottom right",
-        render: ({ id }) => (
-          <Toast nativeID={id} variant="solid" action="error">
-            <ToastTitle>Erro ao criar senha</ToastTitle>
-          </Toast>
-        ),
+        render: ({ id }) => {
+          return (
+            <Toast nativeID={id} variant="solid" action="error">
+              <ToastTitle>Passwords do not match</ToastTitle>
+            </Toast>
+          );
+        },
       });
     }
   };
-
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleState = () => {
-    setShowPassword((showState) => !showState);
+    setShowPassword((showState) => {
+      return !showState;
+    });
   };
   const handleConfirmPwState = () => {
-    setShowConfirmPassword((showState) => !showState);
+    setShowConfirmPassword((showState) => {
+      return !showState;
+    });
   };
   const handleKeyPress = () => {
     Keyboard.dismiss();
     handleSubmit(onSubmit)();
   };
-
+  const router = useRouter();
   return (
     <VStack className="max-w-[440px] w-full" space="md">
       <VStack className="md:items-center" space="md">
@@ -140,15 +119,18 @@ const CreatePasswordWithLeftBackground = () => {
         </Pressable>
         <VStack>
           <Heading className="md:text-center" size="3xl">
-            Recuperação de Senha
+            Create new password
           </Heading>
+          <Text className="md:text-center">
+            Your new password must be different from previously used passwords{" "}
+          </Text>
         </VStack>
       </VStack>
       <VStack className="w-full">
         <VStack space="xl" className="w-full">
           <FormControl isInvalid={!!errors.password}>
             <FormControlLabel>
-              <FormControlLabelText>Nova senha</FormControlLabelText>
+              <FormControlLabelText>Password</FormControlLabelText>
             </FormControlLabel>
             <Controller
               defaultValue=""
@@ -170,12 +152,12 @@ const CreatePasswordWithLeftBackground = () => {
                 <Input>
                   <InputField
                     className="text-sm"
-                    placeholder="Nova senha"
+                    placeholder="Password"
                     value={value}
                     onChangeText={onChange}
                     onBlur={onBlur}
                     onSubmitEditing={handleKeyPress}
-                    enterKeyHint="done"
+                    returnKeyType="done"
                     type={showPassword ? "text" : "password"}
                   />
                   <InputSlot onPress={handleState} className="pr-3">
@@ -190,13 +172,15 @@ const CreatePasswordWithLeftBackground = () => {
                 {errors?.password?.message}
               </FormControlErrorText>
             </FormControlError>
-            <FormControlLabel></FormControlLabel>
+            <FormControlLabel>
+              <FormControlLabelText className="text-typography-500">
+                Must be atleast 8 characters
+              </FormControlLabelText>
+            </FormControlLabel>
           </FormControl>
           <FormControl isInvalid={!!errors.confirmpassword}>
             <FormControlLabel>
-              <FormControlLabelText>
-                Confirme sua nova senha
-              </FormControlLabelText>
+              <FormControlLabelText>Confirm Password</FormControlLabelText>
             </FormControlLabel>
             <Controller
               defaultValue=""
@@ -217,15 +201,16 @@ const CreatePasswordWithLeftBackground = () => {
               render={({ field: { onChange, onBlur, value } }) => (
                 <Input>
                   <InputField
-                    placeholder="Repita a senha"
+                    placeholder="Confirm Password"
                     className="text-sm"
                     value={value}
                     onChangeText={onChange}
                     onBlur={onBlur}
                     onSubmitEditing={handleKeyPress}
-                    enterKeyHint="done"
+                    returnKeyType="done"
                     type={showConfirmPassword ? "text" : "password"}
                   />
+
                   <InputSlot onPress={handleConfirmPwState} className="pr-3">
                     <InputIcon
                       as={showConfirmPassword ? EyeIcon : EyeOffIcon}
@@ -240,13 +225,17 @@ const CreatePasswordWithLeftBackground = () => {
                 {errors?.confirmpassword?.message}
               </FormControlErrorText>
             </FormControlError>
-            <FormControlLabel></FormControlLabel>
+            <FormControlLabel>
+              <FormControlLabelText className="text-typography-500">
+                Both passwords must match
+              </FormControlLabelText>
+            </FormControlLabel>
           </FormControl>
         </VStack>
 
         <VStack className="mt-7 w-full">
           <Button className="w-full" onPress={handleSubmit(onSubmit)}>
-            <ButtonText className="font-medium">Alterar sua senha</ButtonText>
+            <ButtonText className="font-medium">Update Password</ButtonText>
           </Button>
         </VStack>
       </VStack>
