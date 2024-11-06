87915a3c58f79bc3fb24accafa91710d9c63bdfd
Fátima Kraiczyi <fatima.kraiczyi@brf.com>
2024-11-05 21:44:33 -0300

    remove splash screen

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,14 +1,34 @@
 "use client";
-
-import { SplashScreen } from "@/screens/auth/SplashScreen";
-
+import { getToken } from "@/api/token";
+import { LoadingState } from "@/components/sections/LoadingState";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Page = () => {
-  return (
-    <main className="w-full" >
-      <SplashScreen />
-    </main>
-  );
-}
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await getToken();
+
+      if (token) {
+        router.replace("dashboard");
+      } else {
+        router.replace("auth/signin");
+      }
+
+      setLoading(false);
+    };
+
+    checkToken();
+  }, [router]);
+
+  if (loading) {
+    return <LoadingState />;
+  }
+
+  return null;
+};
 
 export default Page;
